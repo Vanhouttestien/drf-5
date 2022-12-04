fa0964b14de36d42c48a4de0ebc1e7667e632ba6
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-12-04 11:58:16 +0000

    add saved_post_id to posts serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,15 +1,26 @@
 from rest_framework import serializers
 from .models import Post
-
+from saved_posts.models import SavedPost
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    saved_post_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-        
+    
+
+    def get_saved_post_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            saved_post = SavedPost.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return saved_post.id if saved_post else None
+        return None
+
     class Meta:
         model = Post
         fields = '__all__'
