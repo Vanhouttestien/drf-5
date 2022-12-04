495349bfdbe0bbdb190fa6eb51f60067280b23e0
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-12-04 17:35:21 +0000

    fix filter savemodel

--- saved_posts/serializers.py
+++ saved_posts/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import SavedPost
 
@@ -9,4 +10,10 @@ class SavedPostSerializer(serializers.ModelSerializer):
         model = SavedPost
         fields = '__all__'
     
-
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate.'
+            })
