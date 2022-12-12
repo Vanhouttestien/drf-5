2b30acafa282deceff1e47473b1d147704190bb7
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-12-12 10:47:03 +0000

    import logout_route in views

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 
 urlpatterns = [
